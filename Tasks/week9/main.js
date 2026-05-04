b115e5fb5d6b87fd2da943ca90a99051ceaea05e
Mykhailo-82 <mykhailo.ovchinnikov.28@gmail.com>
2026-05-04 09:12:40 +0300

    Week 9

--- Tasks/week9/main.js
+++ Tasks/week9/main.js
@@ -20,40 +20,64 @@ document.addEventListener("DOMContentLoaded", () => {
 
     const { scene, camera, renderer } = mindarThree;
 
-    
+
     renderer.sortObjects = true;
     renderer.autoClear = false;
 
-    
+
     scene.add(new THREE.HemisphereLight(0xffffff, 0xbbbbff, 1.5));
+
     const directLight = new THREE.DirectionalLight(0xffffff, 1);
     directLight.position.set(0, 1, 1);
     scene.add(directLight);
 
-    
     const faceMesh = mindarThree.addFaceMesh();
+
     faceMesh.material = new THREE.MeshBasicMaterial({
       colorWrite: false,
-      depthWrite: true,
-      side: THREE.DoubleSide
+      depthWrite: true
     });
-    faceMesh.renderOrder = 0; 
+
+    faceMesh.renderOrder = 0;
     scene.add(faceMesh);
 
-    
     const loader = new GLTFLoader();
+
+
+    loader.load("../../assets/models/headOccluder.glb", (gltf) => {
+      const head = gltf.scene;
+
+      head.scale.set(1.1, 0.8, 1.2);
+      head.position.set(0, -2.3, 7);
+
+      head.traverse((child) => {
+        if (child.isMesh) {
+          child.material = new THREE.MeshBasicMaterial({
+            // colorWrite: '#ffffff',
+            colorWrite: false,
+            depthWrite: true,
+            depthTest: true
+          });
+
+          child.renderOrder = 0;
+        }
+      });
+
+      faceMesh.add(head);
+    });
+
     loader.load("../../assets/models/hat.glb", (gltf) => {
       const hatModel = gltf.scene;
-      hatModel.scale.set(4.0, 4.0, 4.0); 
-      hatModel.position.set(0, 0.4, -0.6); 
-      hatModel.rotation.set(0, 180, 0); 
+
+      hatModel.scale.set(4.0, 4.0, 4.0);
+      hatModel.position.set(0, 0.4, -0.6);
+      hatModel.rotation.set(0, 180, 0);
 
       hatModel.traverse((child) => {
         if (child.isMesh) {
-          child.renderOrder = 999; 
+          child.renderOrder = 10;
           child.material.depthTest = true;
           child.material.depthWrite = true;
-          child.material.transparent = false;
         }
       });
 
@@ -61,7 +85,6 @@ document.addEventListener("DOMContentLoaded", () => {
       anchor.group.add(hatModel);
     });
 
-    
     if (isVR) {
       const geometry = new THREE.SphereGeometry(0.01, 32, 16);
       const material = new THREE.MeshBasicMaterial({
@@ -79,16 +102,12 @@ document.addEventListener("DOMContentLoaded", () => {
 
     await mindarThree.start();
 
-    
     camera.near = 0.01;
     camera.updateProjectionMatrix();
 
-    
     if (isVR) {
       const video = document.querySelector("video");
-      if (video) {
-        video.style.display = "none";
-      }
+      if (video) video.style.display = "none";
     }
 
     renderer.setAnimationLoop(() => {
